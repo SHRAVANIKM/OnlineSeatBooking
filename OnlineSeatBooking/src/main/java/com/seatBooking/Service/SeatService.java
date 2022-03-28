5c7370f5fdb3b82c85cd4b483f1019b19472dc2b
shrakm <shrakm@INLEN8520027372.corp.capgemini.com>
2022-03-28 15:09:32 +0530

    changes in class

--- OnlineSeatBooking/src/main/java/com/seatBooking/Service/SeatService.java
+++ OnlineSeatBooking/src/main/java/com/seatBooking/Service/SeatService.java
@@ -4,9 +4,12 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
 import com.seatBooking.Model.Seat;
 import com.seatBooking.Repository.SeatRepo;
 
+@Service
 @Component
 public class SeatService {
 
