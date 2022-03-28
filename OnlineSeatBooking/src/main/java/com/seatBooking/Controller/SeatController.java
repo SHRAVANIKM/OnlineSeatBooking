1361776d47608a2d1b2915cf4c123fa8c0bd7fc9
shrakm <shrakm@INLEN8520027372.corp.capgemini.com>
2022-03-28 15:14:35 +0530

    changes

--- OnlineSeatBooking/src/main/java/com/seatBooking/Controller/SeatController.java
+++ OnlineSeatBooking/src/main/java/com/seatBooking/Controller/SeatController.java
@@ -1,14 +1,15 @@
 package com.seatBooking.Controller;
 
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.seatBooking.Exception.ResourceNotFoundException;
 import com.seatBooking.Model.Seat;
 import com.seatBooking.Service.SeatService;
