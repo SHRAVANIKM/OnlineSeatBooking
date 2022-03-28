8dd8d4862f2521464c025ca8970ffe75d5cc4cd3
shrakm <shrakm@INLEN8520027372.corp.capgemini.com>
2022-03-28 15:17:18 +0530

    changes

--- OnlineSeatBooking/src/main/java/com/seatBooking/Exception/GlobalExceptionHandler.java
+++ OnlineSeatBooking/src/main/java/com/seatBooking/Exception/GlobalExceptionHandler.java
@@ -1,16 +1,11 @@
 package com.seatBooking.Exception;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
-import org.springframework.http.HttpHeaders;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
